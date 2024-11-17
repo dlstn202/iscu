abc7b2e3ce487a79e229de81e1863b1fdc0f2a1c
psm <dlstn202@naver.com>
2024-11-17 18:00:47 +0900

    start

---
+++ psm/src/main/java/portpolio/psm/PsmApplication.java
@@ -0,0 +1,13 @@
+package portpolio.psm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsmApplication.class, args);
+	}
+
+}
